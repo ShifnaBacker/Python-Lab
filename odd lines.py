c48d63ba4847d737f173b56ab3e1b8e55e242ac6
ShifnaBacker <shifnanilu@gmail.com>
2025-11-18 11:59:56 +0530

    file

---
+++ odd lines.py
@@ -0,0 +1,9 @@
+file1 = open("demo.txt","r")
+file2 = open("new.txt","w")
+count = 0
+for line in file1:
+    count += 1
+    if (count % 2 != 0):
+        file2.write(line)
+file1.close()
+file2.close()
